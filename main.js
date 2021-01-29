2a47d489bdeadb6ef4109072bbf812e200e77d9c
celsa Vilanculo <vilanculo.celsa@gmail.com>
2021-01-30 00:12:49 +0200

    adicao de funcionalidades para funcoes com argumento como alteracao de fonte, titulos, cor de texto, tamanho

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
     var editorDoc;
+    var showingSourceCode = false;
         function InitEditable () {
-            var editor = document.getElementById ("editor");
+            var editor = document.getElementById ("editorField");
             editorDoc = editor.contentWindow.document;
             var editorBody = editorDoc.body;
 
@@ -25,4 +26,22 @@
         var x=comando;
         //alert(x);
         editorDoc.execCommand (x, false, null);
+        }
+        
+        function toggleSource(){
+        	if(showingSourceCode){
+        		editor.document.getElementByTagName('body')[0].innerHTML=editor.document.getElementByTagName('body')[0].textContent;
+        		showingSourceCode =  false;       	
+        	}else{
+        		editor.document.getElementByTagName('body')[0].textContent=editor.document.getElementByTagName('body')[0].innerHTML;
+        		showingSourceCode = true;
+        	}
+        }
+
+        function execCmdWithArg(comando, argumento){
+            var y = comando;
+            var z = argumento ;
+            //document.getElementById('test').innerHTML =y +' '+ z;
+            editorDoc.execCommand (y, false, z);
+
         }
